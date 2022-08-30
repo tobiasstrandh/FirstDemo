c2748dff0e222dc0e309c361fafcd87960a47eb7
tobiasstrandh <tobias.strandh@iths.se>
2022-08-30 16:04:48 +0200

    update

--- FirstDemo/FirstDemo/Program.cs
+++ FirstDemo/FirstDemo/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, My name is Tobias!");
+﻿string name;
+string age;
+string frukost;
+
+Console.WriteLine("Hej, Vad är ditt förnamn?");
+name = Console.ReadLine();
+
+Console.WriteLine("Hur gammal är du?");
+age = Console.ReadLine();
+
+Console.WriteLine("Åt du frukost idag? Ja eller nej?");
+frukost = Console.ReadLine();
+
+Console.WriteLine("Hej! " + name + " " + age + " år gammal");
+
+//Console.WriteLine("Hello, My name is Tobias!");
